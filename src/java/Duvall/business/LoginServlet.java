fb3f085e642d120cb96209b5e2e6f88c067765a2
dduv <dduv@LAPTOP-R6EV29RT>
2018-04-19 17:35:13 -0400

    Merge origin/master 04/19/2018 TOBA correction - also added / to Login.jsp for LoginServlet.java

--- src/java/Duvall/business/LoginServlet.java
+++ src/java/Duvall/business/LoginServlet.java
@@ -29,7 +29,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String url = "Login.jsp";
+        String url = "/Login.jsp";
 
         // get current action
         String action = request.getParameter("action");
